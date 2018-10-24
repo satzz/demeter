2a54defee15c97c330e0a312fe1261f81037db3f
satzz <satzz21@gmail.com>
2018-10-24 18:58:56 +0900

    setup pins

--- motors.py
+++ motors.py
@@ -3,20 +3,29 @@ import RPi.GPIO as GPIO
 import time
 import fileinput
 
-# GPIO.setmode(GPIO.BOARD)
 GPIO.setmode(GPIO.BCM)
 
+# setup input pins
 switchLeftPIN = 27
+switchRightPIN = 22
 GPIO.setup(switchLeftPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switchRightPIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# setup output pins
+servoPIN = 18 # slider
+rotatePIN = 12
+vibePIN = 17
+GPIO.setup(vibePIN, GPIO.OUT)
+GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(rotatePIN, GPIO.OUT)
+
 while True:
-    input_state = GPIO.input(switchLeftPIN)
+    input_state = GPIO.input(switchRightPIN)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
 
 
-vibePIN = 17 # vibe
-GPIO.setup(vibePIN, GPIO.OUT)
 
 # while True:
 #   GPIO.output(vibePIN, True)
@@ -25,10 +34,6 @@ GPIO.setup(vibePIN, GPIO.OUT)
 #   GPIO.output(vibePIN, False)
 #   print 'vibe low'
 #   time.sleep(0.5)
-
-servoPIN = 18 # slider
-#servoPIN = 12 # rorate
-GPIO.setup(servoPIN, GPIO.OUT)
 
 pwmFreq = 50
 servo = GPIO.PWM(servoPIN, pwmFreq)
